4da940f77df385dac88733541cc136606a4f2e9c
Aarnav Lokesh <aarnav1lokesh@gmail.com>
2025-08-12 23:34:56 +0530

    Update Next.js configuration for static export and bump version to 1.0.0

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+const nextConfig = {};
 
-const nextConfig = {
-  output: 'export',  // enables static HTML export
-  basePath: isProd ? '/My-Portfolio' : '',
-  assetPrefix: isProd ? '/My-Portfolio/' : '',
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
